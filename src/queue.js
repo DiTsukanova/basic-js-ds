a27a6dd8f915b8439e68af2a56b1d7ddc8e1dfb6
Diana Tsukanova <dtsukanova5@gmail.com>
2022-10-18 01:30:07 +0300

    feat: finish  tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -12,25 +12,46 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.enqueue(3); // adds the element to the queue
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
- */
-class Queue {
+ **/
 
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+//{ value: 3, next: null }
+class Queue {
+  constructor() {
+      this.head = null;
+      this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+      return this.head;
+  }
+
+  enqueue(value) {
+      const lastNodeAdded = new ListNode(value);
+      if (!this.head) {
+          this.head = lastNodeAdded;
+      } else {
+          this.tail.next = lastNodeAdded;
+      }
+      this.tail = lastNodeAdded;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+      if (this.head) {
+          const { value, next } = this.head;
+          this.head = next;
+          return value;
+      }
   }
 }
+
 
 module.exports = {
   Queue
 };
+
+const queue = new Queue();
+
+// queue.enqueue(1); // adds the element to the queue
+// queue.enqueue(3); // adds the element to the queue
+// // queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+queue.getUnderlyingList() // returns { value: 3, next: null }
